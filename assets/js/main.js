f0f91d35e27402407ed6ad7b7bca50a741f330e3
Johannes Peter <joshadow@gmail.com>
2018-04-09 02:49:12 -0700

    various cleanup; Began work on gist upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,98 +5,111 @@ const gist_form     = query('#gist-form');
 const gist_field    = query('#gist-field');
 const reply_content = query('#reply-content');
 
-let gist_api_url = "https://api.github.com/gists";
+const gist_upload = $("#gist-upload");
 
-const log = (prefix, object) => {
-  console.log(`${prefix}: ${JSON.stringify(object, null, 4)}`);
+// storing params for chrome 65 bug workaround - https://stackoverflow.com/q/49723782
+const gist_pattern = gist_field.getAttribute("pattern");
+
+
+// Form input validation
+gist_field.oninvalid = (event) => {
+  event.target.setCustomValidity("Enter a valid Gist ID (32 hex characters)");
 }
 
-const updateDisplay = (prefix, object) => {
-  let obj_str = JSON.stringify(object, null, 4);
-  reply_content.textContent = `${prefix}:\n${obj_str}`;
-};
+gist_field.oninput = (event) => {
+  let slash_pos = gist_field.value.lastIndexOf("/");
+  if(slash_pos != -1)
+  {
+    let old_val = gist_field.value;
+    let new_val = old_val.substring(slash_pos+1);
+
+    gist_field.focus();
+    gist_field.value = new_val;
+    return;
+  }
+  event.target.setCustomValidity(""); 
+  event.target.removeAttribute("pattern");
+}
+
+gist_field.onchange = (event) => {
+  let slash_pos = gist_field.value.lastIndexOf("/")
+  event.target.setAttribute("pattern", gist_pattern);
+}
+
 
 // Form submits a gist id to load
 gist_form.addEventListener('submit', (e) => {
   e.preventDefault();
-  loadGist(gist_field.value);
+  let id = gist_field.value;
+  UpdateDisplay(`loading gist ${id}`);
+  SetURLGistID(id);
+  FetchGistData(id)
+    .then(
+      (vals)    => { UpdateDisplay("SUCCESS", SimplifyGistData(vals) ) },
+      (reasons) => { UpdateDisplay("FAILURE", reasons);}
+    );
 });
 
-// Form input validation
-gist_field.oninvalid = event => {
-  event.target.setCustomValidity("Enter a valid Gist ID (32 hex characters)");
+
+// Button to upload gist content
+gist_upload.on('click', (e) => {
+  
+});
+
+
+
+
+
+
+const SimplifyGistData = (data) => {
+  let files = data["files"];
+  let file_data = {}
+  for (var key in files) {
+    file_data[key] = {
+        filename: files[key]["filename"],
+        content:  files[key]["content"],
+        language: files[key]["language"]
+      };
+  }
+
+  return {
+    url:         data.url,
+    id:          data.id,
+    description: data.description,
+    files:       file_data,
+  }
 }
 
 
-
-const SetURLGistID = id => {
+// Query string logic
+const SetURLGistID = (id) => {
   if (history.pushState) {
     var newurl = window.location.protocol + "//" + window.location.host + window.location.pathname;
-    if(id) newurl = `${newurl}?gist=${id}`;
+    if(id !== undefined) 
+      newurl = `${newurl}?gist=${id}`;
     window.history.replaceState({path:newurl},'',newurl);
   }
 }
 
-const GetURLGistID = () => {
-  return new URLSearchParams(window.location.search).get("gist");
+
+// A basic pretty printing logging function
+const log = (prefix, object) => {
+  if(object === undefined) { console.log(prefix);}
+  else console.log(`${prefix}: ${JSON.stringify(object, null, 4)}`);
 }
+
+const UpdateDisplay = (prefix, object) => {
+  log(prefix, object);
+  if(object === undefined) reply_content.textContent = prefix;
+  else reply_content.textContent = `${prefix}:\n${JSON.stringify(object, null, 4)}`;
+};
+
 
 
 // Get id from query string
-let gist_id = GetURLGistID();
+let gist_id = GetQueryParam("gist");
 if(gist_id !== null && gist_id.length === 32) {
-    gist_field.value = gist_id;
+  gist_field.value = gist_id;
 } else {
   SetURLGistID();
 }
-
-
-const loadGist = id => {
-  $.get({
-    url: `${gist_api_url}/${id}`,
-  }).done(reply => {
-    SetURLGistID(id);
-    getFileContent(reply["files"], (files) => {
-      updateDisplay("SUCCESS", files);
-    });
-  }).fail(err => {
-    updateDisplay("ERROR", err);
-  });
-}
-
-// Github API returns content of all files in gist but some may be truncated.
-// Asynchronously fetches and passes map of filename to content to callback.
-const getFileContent = (data, callback) => {
-  let files    = {}; // Files and their content.
-  let to_fetch = {}; // Files that still need to be fetched
-
-  for (var key in data) {
-    if(data[key]["truncated"])
-      to_fetch[key] = data[key]["raw_url"];
-    else
-      files[key]    = data[key]["content"];
-  }
-
-  // All complete, no need to async fetch
-  if(Object.keys(to_fetch).length === 0)
-    callback(files);
-  
-  // Use promises to GET content of each file
-  let promises = [];
-  for(let filename in to_fetch) {
-    let url = to_fetch[filename];
-    let promise = new Promise( (resolve, reject) => {
-      $.get(url)
-        .done( res => { resolve([filename, res]); } )
-        .fail( err => {  reject([filename, err]); } );
-    });
-    promises.push(promise);
-  }
-
-  // Run promises and consolidate files object
-  Promise.all(promises).then( (vals) => {
-    for(let item in vals)
-      files[vals[item][0]] = vals[item][1];
-    callback(files);
-  });
-};
